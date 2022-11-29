2e57b352241aa8c712aa7d1530e15bc693ca0b99
yunoim <yunoim@naver.com>
2022-11-30 01:45:00 +0900

    Sale and Purchase

--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -26,4 +26,29 @@ contract SaleAnimalToken {
 
         onSaleAnimalTokenArray.push(_animalTokenId);
     }
+
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+
+        require(price > 0, "Animal token not sale.");
+        require(price <= msg.value, "Caller sent lower than price.");
+        require(animalTokenOwner != msg.sender, "Caller is animal token owner.");
+
+        payable(animalTokenOwner).transfer(msg.value);
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+
+        animalTokenPrices[_animalTokenId] = 0;
+
+        for(uint256 i=0; i< onSaleAnimalTokenArray.length; i++) {
+            if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
+                onSaleAnimalTokenArray.pop();
+            }
+        }
+    }
+
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
+    }
 }
