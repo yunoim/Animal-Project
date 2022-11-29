34fb5f9c97d9b5c4011b5619b831897c0384d0be
yunoim <yunoim@naver.com>
2022-11-30 01:25:39 +0900

    fixed

--- SaleAnimalToken.sol
+++ SaleAnimalToken.sol
@@ -12,12 +12,18 @@ contract SaleAnimalToken {
 
     mapping(uint256 => uint256) public animalTokenPrices;
 
+    uint256[] public onSaleAnimalTokenArray;
+
     function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
-        address animalTokenOwner = mintAnimalTokenAddress.ownerof(_animalTokenId);
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
 
         require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
         require(_price >0, "Price is zero or lower.");
         require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale.");
-        require(mintAnimalTokenAdress.isApprovedForAll(animalTokenOwner, address(this)),"Animal token owner did not approve token.");
+        require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)),"Animal token owner did not approve token.");
+
+        animalTokenPrices[_animalTokenId] = _price;
+
+        onSaleAnimalTokenArray.push(_animalTokenId);
     }
 }
